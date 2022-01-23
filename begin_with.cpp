579bcd96bdd219e6f22bc999fe48bf0f9377f37d
Layotiver <layotiver@mail.ustc.edu.cn>
2022-01-23 19:17:34 +0800

    初始化

---
+++ begin_with.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cctype>
+#include <set>
+#include <map>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+#ifdef chenhao
+    freopen("chenhao.txt", "r", stdin);
+#endif
+}
